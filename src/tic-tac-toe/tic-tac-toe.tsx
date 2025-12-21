a49b03ded411f93acfe49d65ce179efd5200b3b0
Uladzimir Pantsiukhou <upantsiukhou@slb.com>
2025-12-21 16:22:32 +0100

    small homeworks

--- src/tic-tac-toe/tic-tac-toe.tsx
+++ src/tic-tac-toe/tic-tac-toe.tsx
@@ -2,21 +2,41 @@ import { useState, type MouseEventHandler } from "react";
 
 export function Square({
   value,
+  isHighlighted,
   onSquareClick,
 }: {
   value: string | null;
+  isHighlighted: boolean | null;
   onSquareClick: MouseEventHandler<HTMLButtonElement>;
 }) {
   return (
-    <button onClick={onSquareClick} className="square">
+    <button onClick={onSquareClick} className="square" style={{backgroundColor: isHighlighted ? 'yellow' : ''}}>
       {value}
     </button>
   );
 }
 
 export function Board({ xIsNext, squares, onPlay }: any) {
+  const rows = [
+    {
+      id: 0,
+      children: [squares[0], squares[1], squares[2]],
+    },
+    {
+      id: 1,
+      children: [squares[3], squares[4], squares[5]],
+    },
+    {
+      id: 2,
+      children: [squares[6], squares[7], squares[8]],
+    },
+  ];
+
+  console.log("r", rows);
+
   function handleSquareClick(i: number) {
-    if (squares[i] || calculateWinner(squares)) return;
+    console.log("handleSquareClick", i);
+    if (squares[i] || calculateWinner(squares)?.winner) return;
     const nextSquares = squares.slice();
     if (xIsNext) {
       nextSquares[i] = "X";
@@ -26,7 +46,8 @@ export function Board({ xIsNext, squares, onPlay }: any) {
     onPlay(nextSquares);
   }
 
-  const winner = calculateWinner(squares);
+  const {winner, row} = calculateWinner(squares);
+  console.log('ROW', row)
   let status;
   if (winner) {
     status = "Winner: " + winner;
@@ -34,24 +55,32 @@ export function Board({ xIsNext, squares, onPlay }: any) {
     status = "Next player: " + (xIsNext ? "X" : "O");
   }
 
+  const rowsEl = rows.map((r, index) => {
+    return {
+      id: index,
+      children: r.children.map((rowChild, index) => {
+        return (
+          <Square
+            isHighlighted={row.includes(index + r.id * 3)}
+            key={index}
+            value={rowChild}
+            onSquareClick={() => handleSquareClick(index + r.id * 3)}
+          />
+        );
+      }),
+    };
+  });
+
+  console.log(rowsEl);
+
   return (
     <>
       <div className="status">{status}</div>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleSquareClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleSquareClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleSquareClick(2)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleSquareClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleSquareClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleSquareClick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleSquareClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleSquareClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleSquareClick(8)} />
-      </div>
+      {rowsEl.map((row) => (
+        <div className="board-row" key={row.id}>
+          {row.children.map((el) => el)}
+        </div>
+      ))}
     </>
   );
 }
@@ -59,12 +88,14 @@ export function Board({ xIsNext, squares, onPlay }: any) {
 export function TicTacToe() {
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
+  const [movesSort, setMovesSort] = useState<string>("asc");
 
   const xIsNext = currentMove % 2 === 0;
   const currentSquares = history[currentMove];
 
   function handlePlay(nextSquares: number[]) {
     const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    console.log(nextHistory);
     setHistory(nextHistory);
     setCurrentMove(nextHistory.length - 1);
   }
@@ -73,7 +104,16 @@ export function TicTacToe() {
     setCurrentMove(nextMove);
   }
 
+  function handleMovesSort() {
+    if (movesSort === "asc") {
+      setMovesSort("desc");
+    } else if (movesSort === "desc") {
+      setMovesSort("asc");
+    }
+  }
+
   const moves = history.map((squares, move) => {
+    if (move === currentMove) return;
     let description;
     if (move > 0) {
       description = "Go to move #" + move;
@@ -87,13 +127,21 @@ export function TicTacToe() {
     );
   });
 
+  let sortedMoves = [...moves];
+  if (movesSort === "desc") {
+    sortedMoves = [...moves].reverse();
+  }
+
   return (
     <>
       <div className="game-board">
         <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
       <div className="game-info">
-        <ol>{moves}</ol>
+        You are at move {currentMove}
+        <hr />
+        Order <button onClick={() => handleMovesSort()}>{movesSort}</button>
+        <ol>{sortedMoves}</ol>
       </div>
     </>
   );
@@ -113,8 +161,8 @@ function calculateWinner(squares: number[]) {
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+      return { winner: squares[a], row: [a, b, c] };
     }
   }
-  return null;
+  return {winner: null, row: [] as number[]};
 }
